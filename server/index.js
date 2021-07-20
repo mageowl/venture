2222708519ec0e06d432dd8da867151f15492280
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-07-20 16:07:04 -0700

    more things

--- server/index.js
+++ server/index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("u not playing. go play");
+});
+
+server.listen(port);
